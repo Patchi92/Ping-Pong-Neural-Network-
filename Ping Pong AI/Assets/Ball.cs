7e35dc4de1d92b37b88d5ca4e90e90507f86fdb9
Patchi92 <lechieu@hotmail.com>
2015-04-19 02:14:57 +0200

    PingPong

---
+++ Ping Pong AI/Assets/Ball.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	public Vector3 Impulse;
+	public Vector3 PlayerVelY;
+	public Vector3 PlayerTwoVelY;
+
+	float velx;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody>().AddForce(Impulse, ForceMode.Impulse);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter (Collision Info) {
+		if(Info.collider.tag == "PlayerOne") {
+
+			velx = GetComponent<Rigidbody>().velocity.x;
+			velx = velx/2 + Info.collider.attachedRigidbody.velocity.x/3;
+
+			GetComponent<Rigidbody>().AddForce(PlayerVelY, ForceMode.Impulse);
+
+		}
+
+		if(Info.collider.tag == "PlayerTwo") {
+			
+			velx = GetComponent<Rigidbody>().velocity.x;
+			velx = velx/2 + Info.collider.attachedRigidbody.velocity.x/3;
+			
+			GetComponent<Rigidbody>().AddForce(PlayerTwoVelY, ForceMode.Impulse);
+			
+		}
+	}
+}
---
+++ Ping Pong AI/Assets/PlayerControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	float speed = 10f;
+	Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+		if(Input.GetKey(KeyCode.A)) {
+			rb.velocity = new Vector3(speed * -1, 0, 0);
+		} else if(Input.GetKey(KeyCode.D)) {
+			rb.velocity = new Vector3(speed, 0, 0);
+		} else {
+			rb.velocity = new Vector3(0, 0, 0);
+		}
+
+	}
+}
